592af8884623adb8df4c9fada0490201d6cb6b42
Paul <paulkadabo@gmail.com>
2023-07-05 23:09:37 +0300

    claim models added

--- apps/claims/models.py
+++ apps/claims/models.py
@@ -5,18 +5,36 @@ from apps.users.models import User
 
 
 # Create your models here.
-class Claim(AbstractBaseModel):
-    STATE_CHOICES = (
-        ("lodged", "Lodged"),
-        ("awaiting_approval", "Awaiting approval"),
-        ("awaiting_payment", "Awaiting payment"),
-        ("closed", "Closed"),
-        ("paid", "Paid"),
-    )
+STATE_CHOICES = (
+    ("lodged", "Lodged"),
+    ("awaiting_approval", "Awaiting approval"),
+    ("awaiting_payment", "Awaiting payment"),
+    ("closed", "Closed"),
+    ("paid", "Paid"),
+)
 
-    # insured_item = models.ForeignKey(InsuredItem, null=True, on_delete=models.CASCADE)
+
+CONTACT_CHOICES = (
+    ("email", "Email"),
+    ("sms", "SMS"),
+    ("call", "Call",)
+)
+
+class ClaimGenericFieldList(AbstractBaseModel):
+    name = models.CharField(max_length=255)
+    is_file = models.BooleanField(default=False)
+    is_required = models.BooleanField(default=False)
+    is_standard = models.BooleanField(default=True)
+    reasons_allowed = models.JSONField(default=list)
+    claim_against_allowed = models.JSONField(default=list)
+
+    def __str__(self):
+        return self.name
+
+
+class Claim(AbstractBaseModel):
     policy = models.ForeignKey("policies.Policy", null=True, on_delete=models.CASCADE)
-    status = models.CharField(max_length=32)
+    status = models.CharField(max_length=255, default="lodged")
     sub_status = models.CharField(max_length=32, null=True)
     reason = models.TextField()
     sub_reason = models.CharField(max_length=255, null=True)
@@ -30,6 +48,7 @@ class Claim(AbstractBaseModel):
     policy_status_when_lodged = models.CharField(max_length=255, null=True)
     proof_of_payment = models.FileField(upload_to="proof_of_payments/", null=True)
     membership = models.ForeignKey("users.Membership", on_delete=models.CASCADE, null=True)
+    dependent = models.ForeignKey("dependents.Dependent", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.reference_number
@@ -62,3 +81,21 @@ class ClaimAdditionalInfo(AbstractBaseModel):
 
     def __str__(self):
         return self.title
+
+
+class Claimant(AbstractBaseModel):
+    claim = models.OneToOneField(Claim, on_delete=models.SET_NULL, null=True)
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    id_number = models.CharField(max_length=255, null=True)
+    passport_number = models.CharField(max_length=255, null=True)
+    phone_number = models.CharField(max_length=255, null=True)
+    email = models.EmailField(null=True)
+    preferred_contact_method = models.CharField(max_length=255, choices=CONTACT_CHOICES)
+    relationship = models.ForeignKey("users.PolicyHolderRelative", on_delete=models.SET_NULL, null=True)
+    physical_address = models.CharField(max_length=255, null=True)
+    postal_address = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.claim.reference_number
