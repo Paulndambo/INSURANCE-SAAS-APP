be8e6db43bf40d4e77157d0f3364dd2c0041faba
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-09-26 12:15:11 +0300

    customer family members endpoints added

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -2,9 +2,15 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from apps.customer.policies.views import CustomerPolicyViewSet
+from apps.customer.views import (CustomerBeneficiariesViewSet,
+                                 CustomerDependentViewSet,
+                                 CustomerExtendedDependentViewSet)
 
 router = DefaultRouter()
 router.register("policies", CustomerPolicyViewSet, basename="policies")
+router.register("dependents", CustomerDependentViewSet, basename="depedents")
+router.register("beneficiaries", CustomerBeneficiariesViewSet, basename="beneficiaries")
+router.register("extended-dependents", CustomerExtendedDependentViewSet, basename="extended-dependents")
 
 urlpatterns = [
     path("", include(router.urls)),
--- apps/customer/views.py
+++ apps/customer/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from apps.dependents.models import Beneficiary, Dependent
+from apps.dependents.serializers import (BeneficiarySerializer,
+                                         DependentSerializer)
+from apps.payments.models import PaymentLog, PolicyPayment, PolicyPremium
+
 
 # Create your views here.
+class CustomerDependentViewSet(ModelViewSet):
+    queryset = Dependent.objects.all()
+    serializer_class = DependentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        memberships = user.usermembership.all()
+        return self.queryset.filter(dependent_type__in=["Dependent", "dependent"], membership__in=memberships)
+
+
+class CustomerExtendedDependentViewSet(ModelViewSet):
+    queryset = Dependent.objects.all()
+    serializer_class = DependentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        memberships = user.usermembership.all()
+        return self.queryset.filter(dependent_type__in=["extended", "Extended"], membership__in=memberships)
+
+
+class CustomerBeneficiariesViewSet(ModelViewSet):
+    queryset = Beneficiary.objects.all()
+    serializer_class = BeneficiarySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        memberships = user.usermembership.all()
+        print(memberships)
+        return self.queryset.filter(membership__in=memberships)
